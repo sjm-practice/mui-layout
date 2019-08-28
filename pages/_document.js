9a0d4ee0299e77925c5da4fc2e6a1c012dfdf387
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-08-28 12:08:36 -0400

    remove material icons font, no longer used

--- pages/_document.js
+++ pages/_document.js
@@ -20,10 +20,6 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/static/favicon.ico" />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          />
-          <link
-            rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
         </Head>
